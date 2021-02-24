82f8c5a08f277cb293c9ec3341b9b00e2d4618cd
Tommi <tommikokk@gmail.com>
2021-02-25 01:56:21 +0200

    initialize frontend

---
+++ frontend/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return(
+    <div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

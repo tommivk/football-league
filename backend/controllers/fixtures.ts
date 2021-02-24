21ae9e384c9f6c70f51b456e755d229e573dd956
Tommi <tommikokk@gmail.com>
2021-02-24 23:49:42 +0200

    add routes for fixtures

---
+++ backend/controllers/fixtures.ts
@@ -0,0 +1,28 @@
+const fixturesRouter = require('express').Router();
+const pool = require('../pool');
+
+import { Request, Response } from 'express';
+
+fixturesRouter.post('/', async (req: Request, res: Response) => {
+  try {
+    const { homeTeamId, awayTeamId, venueId, gameDate } = req.body;
+    await pool.query(
+      'INSERT INTO fixtures (home_team_id, away_team_id, venue_id, game_date, home_score, away_score, finished) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+      [homeTeamId, awayTeamId, venueId, gameDate, 0, 0, false]
+    );
+    return res.status(200).end();
+  } catch (error) {
+    return console.log(error);
+  }
+});
+
+fixturesRouter.get('/', async (_req: Request, res: Response) => {
+    try {
+        const { rows } = await pool.query('SELECT * FROM fixtures');
+        res.status(200).json(rows);
+    } catch (error) {
+        return console.log(error);
+    }
+})
+
+module.exports = fixturesRouter;
--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,7 @@ const app = express();
 const venuesRouter = require('./controllers/venues');
 const playersRouter = require('./controllers/players');
 const teamsRouter = require('./controllers/teams');
+const fixturesRouter = require('./controllers/fixtures');
 
 const PORT = 8000;
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/api/venues', venuesRouter);
 app.use('/api/players', playersRouter);
 app.use('/api/teams', teamsRouter);
+app.use('/api/fixtures', fixturesRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`)

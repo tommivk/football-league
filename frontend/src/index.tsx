ed89f68cc22eaca801ed087a4af18e8fdeab2299
Tommi <tommikokk@gmail.com>
2021-02-25 23:11:22 +0200

    league table

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = () => {
+interface Props {
+  leagueData: {
+    name: string,
+    goals: string,
+    id: number,
+  }[]
+}
+
+const LeagueTable: React.FC<Props> = ( { leagueData }) => {
   return(
     <div>
+      <table>
+        <tbody>
+          <th>Team</th>
+          <th>Goals</th>
+          {leagueData.map((d) => (
+            <tr>
+              <td>{d.name}</td>
+              <td>{d.goals}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  const [leagueData, setLeagueData] = useState<[]>([]);
+  const [allTeams, setAllTeams] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/fixtures/table').then((res) => setLeagueData(res.data))
+    axios.get('http://localhost:8000/api/teams').then((res) => setAllTeams(res.data));
+  },[])
+
+  return(
+    <div>
+      <LeagueTable leagueData = {leagueData} />
+      <ul>
+        {allTeams.map((team) => (
+          <li>
+            {team.name}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
